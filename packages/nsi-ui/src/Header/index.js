6b81d863137978fb10111025b2a0863255116bfc
lukonin_va <lukonin_va@ursip.ru>
2019-04-01 17:00:10 +0300

    Добавил прижатый к верху хэдер справочника #2

--- packages/nsi-ui/src/Header/index.js
+++ packages/nsi-ui/src/Header/index.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import ViewHeader from './ViewCatalogHeader'
 import { Route, Switch } from 'react-router-dom'
 import { Text, Box, Flex, Button } from '@ursip/design-system'
+import { Sticky } from 'react-sticky'
 
 /** Вот как из отсюда забрать данные из формы?
  * Как вариант - пихать состояние формы в редакс,
@@ -34,7 +35,7 @@ const EditHeader = props => {
       <Text fontSize={3}>Редактирование справочника</Text>
       <Box className="buttonsWrapper">
         <Box width={96} style={{ display: 'inline-block' }} id="editCatalogButtonContainer" />
-        <Box ml={3} width={96} style={{ display: 'inline-block'}}>
+        <Box ml={3} width={96} style={{ display: 'inline-block' }}>
           <Button block type="secondary" onClick={() => history.goBack()}>
             Отмена
           </Button>
@@ -56,18 +57,23 @@ const HeaderWrapper = styled(Flex)`
   height: 87px;
   align-items: center;
   border-bottom: 1px solid #ecebeb;
+  background-color: white;
 `
 
 const Header = () => {
   return (
-    <HeaderWrapper>
-      <Switch>
-        <Route exact path="/nsi/create" component={CreateHeader} />
-        <Route exact path="/nsi/:id/edit" component={EditHeader} />
-        <Route path="/nsi/:id" component={ViewHeader} />
-        <Route exact path="/nsi" component={Placeholder} />
-      </Switch>
-    </HeaderWrapper>
+    <Sticky topOffset={0}>
+      {({ style }) => (
+        <HeaderWrapper style={{ ...style, zIndex: 10, top: 150 }}>
+          <Switch>
+            <Route exact path="/nsi/create" component={CreateHeader} />
+            <Route exact path="/nsi/:id/edit" component={EditHeader} />
+            <Route path="/nsi/:id" component={ViewHeader} />
+            <Route exact path="/nsi" component={Placeholder} />
+          </Switch>
+        </HeaderWrapper>
+      )}
+    </Sticky>
   )
 }
 
