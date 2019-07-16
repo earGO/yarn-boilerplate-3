f759a5555acd5eded1b540b1b38d3918f9d366b3
Logvinov_fv <logvinov_fv@ursip.ru>
2019-07-16 18:24:15 +0300

    made Login module into dynamic one #114

--- packages/modules/src/login/index.js
+++ packages/modules/src/login/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 import PropTypes from 'prop-types'
 import {
 	Absolute,
@@ -10,17 +11,25 @@ import {
 	Flex,
 	Icon
 } from '../../import'
-import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
 import {Helmet} from 'react-helmet'
 import icon from '../assets/icon.svg'
 import * as actions from './module/actions'
 import {selectors} from './module/selectors'
 import {InputLogin, LogoWrapper, Background} from './style'
+import * as LoginModule from './module'
 
-function Login({error, loading, loginStatus, authAction}) {
+function Login({...props}) {
+	/* State methods */
 	const [login, setLogin] = useState('')
 	const [pwd, setPwd] = useState('')
+
+	/* Redux methods */
+	const dispatch = useDispatch()
+	const error = useSelector(selectors.authError)
+	const loading = useSelector(selectors.authLoading)
+	const loginStatus = useSelector(selectors.loginStatus)
+	const authAction = () => dispatch(actions.auth)
 
 	const loginHandler = e => {
 		setLogin(e)
@@ -38,115 +47,117 @@ function Login({error, loading, loginStatus, authAction}) {
 	}
 
 	return (
-		<Box>
-			<Background />
-			<Flex flexDirection="column" alignItems="center">
-				<Helmet>
-					<title>Логин</title>
-				</Helmet>
-				<Flex
-					flexDirection="column"
-					justifyContent="center"
-					alignItems="center"
-					width="100%"
-					height="100vh"
-					maxWidth={360}
-				>
-					<Relative width="100%">
-						<Text
-							fontSize={5}
-							color="highlight"
-							align="center"
-							pb={80}
-						>
-							Сервис поддержки функций <br /> участников
-							реализации <br /> ТИМ-проектов
-						</Text>
-						<Box>
-							<Absolute top={-52} width="100%">
-								<Flex justifyContent="center">
-									<LogoWrapper
-										bg="white"
-										height={90}
-										width={90}
-										boxShadowSize="md"
-									>
-										<img src={icon} height={26} />
-									</LogoWrapper>
-								</Flex>
-							</Absolute>
-
-							<Card
-								width="100%"
-								bg="white"
-								p={4}
-								boxShadowSize="xl"
+		<DynamicModuleLoader modules={[LoginModule.default]}>
+			<Box>
+				<Background />
+				<Flex flexDirection="column" alignItems="center">
+					<Helmet>
+						<title>Логин</title>
+					</Helmet>
+					<Flex
+						flexDirection="column"
+						justifyContent="center"
+						alignItems="center"
+						width="100%"
+						height="100vh"
+						maxWidth={360}
+					>
+						<Relative width="100%">
+							<Text
+								fontSize={5}
+								color="highlight"
+								align="center"
+								pb={80}
 							>
-								<form onSubmit={submitAuth}>
-									<Box mt={4}>
-										<Text py={2}>Логин</Text>
-										<InputLogin
-											width={1}
-											value={login}
-											onChange={loginHandler}
-										/>
-									</Box>
-
-									<Box my={3}>
-										<Text py={2}>Пароль</Text>
-										<InputLogin
-											type="password"
-											width={1}
-											value={pwd}
-											onChange={passwordHandler}
-										/>
-									</Box>
-
-									<Flex
-										justifyContent="center"
-										width="100%"
-										flexDirection="column"
-									>
-										{loading ? (
-											<Button mt={4} disabled block>
-												<Icon
-													spin
-													name="spinner"
-													mr={2}
-													top={1}
-												/>
-												Loading...
-											</Button>
-										) : (
-											<Button
-												mt={4}
-												onClick={submitAuth}
-												block
-											>
-												Войти
-											</Button>
-										)}
-										{error && (
-											<Text
-												mt={2}
-												color="error"
-												align="center"
-											>
-												{error !== null
-													? 'Неправильный логин или пароль'
-													: ' '}
-											</Text>
-										)}
+								Сервис поддержки функций <br /> участников
+								реализации <br /> ТИМ-проектов
+							</Text>
+							<Box>
+								<Absolute top={-52} width="100%">
+									<Flex justifyContent="center">
+										<LogoWrapper
+											bg="white"
+											height={90}
+											width={90}
+											boxShadowSize="md"
+										>
+											<img src={icon} height={26} />
+										</LogoWrapper>
 									</Flex>
-								</form>
-							</Card>
-						</Box>
-					</Relative>
+								</Absolute>
 
-					{loginStatus ? <Redirect to="/nsi" /> : null}
+								<Card
+									width="100%"
+									bg="white"
+									p={4}
+									boxShadowSize="xl"
+								>
+									<form onSubmit={submitAuth}>
+										<Box mt={4}>
+											<Text py={2}>Логин</Text>
+											<InputLogin
+												width={1}
+												value={login}
+												onChange={loginHandler}
+											/>
+										</Box>
+
+										<Box my={3}>
+											<Text py={2}>Пароль</Text>
+											<InputLogin
+												type="password"
+												width={1}
+												value={pwd}
+												onChange={passwordHandler}
+											/>
+										</Box>
+
+										<Flex
+											justifyContent="center"
+											width="100%"
+											flexDirection="column"
+										>
+											{loading ? (
+												<Button mt={4} disabled block>
+													<Icon
+														spin
+														name="spinner"
+														mr={2}
+														top={1}
+													/>
+													Loading...
+												</Button>
+											) : (
+												<Button
+													mt={4}
+													onClick={submitAuth}
+													block
+												>
+													Войти
+												</Button>
+											)}
+											{error && (
+												<Text
+													mt={2}
+													color="error"
+													align="center"
+												>
+													{error !== null
+														? 'Неправильный логин или пароль'
+														: ' '}
+												</Text>
+											)}
+										</Flex>
+									</form>
+								</Card>
+							</Box>
+						</Relative>
+
+						{loginStatus ? <Redirect to="/nsi" /> : null}
+					</Flex>
 				</Flex>
-			</Flex>
-		</Box>
+			</Box>
+		</DynamicModuleLoader>
 	)
 }
 
@@ -157,11 +168,4 @@ Login.propTypes = {
 	loginStatus: PropTypes.bool
 }
 
-export default connect(
-	state => ({
-		error: selectors.authError(state),
-		loading: selectors.authLoading(state),
-		loginStatus: selectors.loginStatus(state)
-	}),
-	{authAction: actions.auth}
-)(Login)
+export default Login
